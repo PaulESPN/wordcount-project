a796b2bdbb0d16e68367d7520b162229eeb16d8e
PaulESPN <espinozapaul53@yahoo.com>
2020-02-06 19:33:19 -0600

    Word Count, Basic functionallity

---
+++ wordcount/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+
+    worddict = {}
+
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'worddict': worddict.items()})
+
+def about(request):
+    return render(request, 'about.html')
